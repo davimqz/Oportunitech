bfc1cd5ca5d84566521592b44e12f0d4e1cfac8d
Davi Marques <dmo3@cesar.school>
2025-09-25 09:23:15 -0300

    [Init] - Initial Commit

---
+++ trabalho-bd/src/main/java/br/com/cs/oportunitech/trabalho_bd/TrabalhoBdApplication.java
@@ -0,0 +1,13 @@
+package br.com.cs.oportunitech.trabalho_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoBdApplication.class, args);
+	}
+
+}
