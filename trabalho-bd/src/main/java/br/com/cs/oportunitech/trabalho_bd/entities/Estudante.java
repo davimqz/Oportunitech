68225fcbcee88ce91a74adfb30c0c42ee6642053
Davi Marques <dmo3@cesar.school>
2025-09-26 10:36:31 -0300

    [Feat] - Estudante entity

---
+++ trabalho-bd/src/main/java/br/com/cs/oportunitech/trabalho_bd/entities/Estudante.java
@@ -0,0 +1,30 @@
+package br.com.cs.oportunitech.trabalho_bd.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Setter
+@Getter
+@Table (name = "tb_estudante")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Estudante {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String email;
+    private List<String> telefones;
+    private String nome;
+}
