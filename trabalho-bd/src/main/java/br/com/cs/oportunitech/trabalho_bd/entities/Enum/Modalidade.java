358c23792fa7fd8d4850844bee00b4dec947ea96
Davi Marques <dmo3@cesar.school>
2025-09-26 11:04:27 -0300

    [Feat] - Vaga entity

---
+++ trabalho-bd/src/main/java/br/com/cs/oportunitech/trabalho_bd/entities/Enum/Modalidade.java
@@ -0,0 +1,7 @@
+package br.com.cs.oportunitech.trabalho_bd.entities.Enum;
+
+public enum Modalidade {
+    REMOTO,
+    PRESENCIAL, 
+    HIBRIDO;
+}
---
+++ trabalho-bd/src/main/java/br/com/cs/oportunitech/trabalho_bd/entities/Vaga.java
@@ -0,0 +1,35 @@
+package br.com.cs.oportunitech.trabalho_bd.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+import br.com.cs.oportunitech.trabalho_bd.entities.Enum.Modalidade;
+
+@Entity
+@Setter
+@Getter
+@Table (name = "tb_vaga")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vaga {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.UUID)
+    private UUID cod_vaga;
+    private String titulo;
+    private String descricao;
+    private int carga_horaria;
+    private Modalidade modalidades;
+    
+} 
+    
+
+
