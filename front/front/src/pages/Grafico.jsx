f7fa530e553ec90df3125ce8e0ee79919997bc4d
CaioLira18 <caiozao1212@gmail.com>
2025-09-28 20:09:38 -0300

    [feat] - Update Front

--- front/front/src/pages/Grafico.jsx
+++ front/front/src/pages/Grafico.jsx
@@ -26,8 +26,25 @@ const Grafico = () => {
       <div className="graficoContainer">
         <div className="cabecalhoGrafico">
             <h1>Graficos</h1>
+            <div className="graficosTypes">
+                <div className="boxType">
+                    <a href="#barras">
+                    <i class="fa-solid fa-chart-simple"></i>
+                    <p>Barras</p></a>
+                </div>
+                <div className="boxType">
+                    <a href="#pizza">
+                    <i class="fa-solid fa-chart-pie"></i>
+                    <p>Pizza</p></a>
+                </div>
+                <div className="boxType">
+                    <a href="#dispercao">
+                    <i class="fa-solid fa-chart-line"></i>
+                    <p>Disperção</p></a>
+                </div>
+            </div>
         </div>
-        <div className="graficoBox">
+        <div className="graficoBox" id='barras'>
             {/* Barras */}
             <div className="typeGrafico">
                 <i class="fa-solid fa-chart-simple fa-2x"></i>
@@ -55,7 +72,7 @@ const Grafico = () => {
 
         </div>
 
-        <div className="graficoBox">
+        <div className="graficoBox" id='pizza'>
             {/* Pizza */}
             <div className="typeGrafico">
                 <i class="fa-solid fa-chart-pie fa-2x"></i>
@@ -82,6 +99,35 @@ const Grafico = () => {
           )}
 
         </div>
+
+        <div className="graficoBox" id='dispercao'>
+            {/* Pizza */}
+            <div className="typeGrafico">
+                <i class="fa-solid fa-chart-pie fa-2x"></i>
+                <h1>Grafico de Pizza</h1>
+            </div>
+            {graficos.length > 0 ? (
+                graficos.map((grafico) => (
+                <li key={grafico.id}>
+                    {grafico.type == "PIZZA" && (
+                    <div className="graficoIndividual">
+                        <div className="graficoIndividualImage">
+                            <img src={grafico.image} alt="" />
+                        </div>
+                        <div className="graficoIndividualInformations">
+                            <h1>{grafico.name}</h1>
+                            <p>{grafico.description}</p>
+                        </div>
+                    </div>
+                    )}
+                </li>
+                ))
+            ) : (
+            <p>Nenhum grafico encontrado.</p>
+          )}
+
+        </div>
+
       </div>
     </div>
   )
