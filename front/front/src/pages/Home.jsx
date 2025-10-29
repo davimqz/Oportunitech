bb9549114ce75f4d96dca2bf6584207551ee257f
CaioLira18 <caiozao1212@gmail.com>
2025-10-29 19:16:15 -0300

    [feat] - Update

--- front/front/src/pages/Home.jsx
+++ front/front/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const API_URL = "https://oportunitech-1.onrender.com/api" || "http://localhost:8080/api";
 
   useEffect(() => {
-    fetch(`${API_URL}/api/vagas`)
+    fetch(`${API_URL}/vagas`)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Erro ao buscar itens.");
@@ -26,19 +26,25 @@ const Home = () => {
     <div>
         <h1>Home</h1>
 
+        {vagas.map((vaga) => (
+
+        
         <div className="vagaContainer">
           <div className="boxContainer">
               <div className="logoText">
-                <h1>Nome Vaga</h1>
-                <img src="" alt="j" />
+                <h1>{vaga.titulo}</h1>
+                <img src={vaga.logoLink} alt="j" />
               </div>
-              <p>Nome Empresa</p>
+              <p>{vaga.descricao}</p>
+              <p>{vaga.empresa}</p>
             
             <div className="buttonVaga">
               <button>Sobre a Vaga</button>
             </div>
           </div>
         </div>
+        ))}
+
       </div>
   )
 }
