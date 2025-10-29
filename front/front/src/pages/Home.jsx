72fdcac79bda738740e5461577e6595817ee5e22
CaioLira18 <caiozao1212@gmail.com>
2025-10-29 15:37:21 -0300

    [feat] - Update

--- front/front/src/pages/Home.jsx
+++ front/front/src/pages/Home.jsx
@@ -3,10 +3,22 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div className="containerHome">
         <h1>Home</h1>
+
+        <div className="vagaContainer">
+          <div className="boxContainer">
+              <div className="logoText">
+                <h1>Nome Vaga</h1>
+                <img src="" alt="j" />
+              </div>
+              <p>Nome Empresa</p>
+            
+            <div className="buttonVaga">
+              <button>Sobre a Vaga</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   )
 }
 
